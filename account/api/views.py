dbf6ae2b29dce741338992b213f8b4da3ba0b314
Elvin Seyidov <alvinseyidov@gmail.com>
2025-02-09 11:18:12 +0400

    update

--- account/api/views.py
+++ account/api/views.py
@@ -174,7 +174,7 @@ class InterestListView(ListAPIView):
         search_query = self.request.query_params.get('search', None)
         if search_query and len(search_query) >= 1:
             return Interest.objects.filter(name__icontains=search_query).order_by('name')[:5]
-        return Interest.objects.none()
+        return Interest.objects.all()
 
 
 
